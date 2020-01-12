99f885da11de3ea681c446375f4b871a758f52f9
afeezdev <afeezdev@gmail.com>
2020-01-12 19:10:38 +0100

    created project with react app and added hompepage files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import HomePag from './homepage.component'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <HomePag />
+      </div>
+    );
+  }
+}
+
+export default App;
