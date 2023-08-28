150518270d857fbce50c361ca874bb8e30f1a090
Afeez Abdulyekeen <46626644+afeezdev@users.noreply.github.com>
2023-08-28 03:13:21 +0100

    Update authentication.styles.jsx

--- src/routes/authentication/authentication.styles.jsx
+++ src/routes/authentication/authentication.styles.jsx
@@ -4,7 +4,7 @@ export const AuthenticationContainer = styled.div`
   display: flex;
   justify-content: space-between;
   margin: 30px auto;
-  margin-top: 100px;
+  margin-top: 80px;
 
   @media only screen and (max-width: 800px) {
     flex-direction: column;
